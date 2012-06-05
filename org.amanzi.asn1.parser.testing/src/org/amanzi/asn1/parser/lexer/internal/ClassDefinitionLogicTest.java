0ba21dd1c0010d2cbdd9b560b04fd38865f06baf
Nikolay Lagutko <nikolay.lagutko@amanzitel.com>
2012-06-05 09:43:38 +0300

    Nes tests for ClassDefinition

--- org.amanzi.asn1.parser.testing/src/org/amanzi/asn1/parser/lexer/internal/ClassDefinitionLogicTest.java
+++ org.amanzi.asn1.parser.testing/src/org/amanzi/asn1/parser/lexer/internal/ClassDefinitionLogicTest.java
@@ -14,25 +14,34 @@
 package org.amanzi.asn1.parser.lexer.internal;
 
 import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
 
 import org.amanzi.asn1.parser.IStream;
+import org.amanzi.asn1.parser.lexer.exception.ErrorReason;
+import org.amanzi.asn1.parser.lexer.exception.SyntaxException;
 import org.amanzi.asn1.parser.lexer.impl.ClassDefinition;
 import org.amanzi.asn1.parser.lexer.impl.Enumerated;
+import org.amanzi.asn1.parser.lexer.impl.IClassDescription;
 import org.amanzi.asn1.parser.lexer.impl.IClassDescription.ClassDescriptionType;
+import org.amanzi.asn1.parser.lexer.impl.ILexem;
 import org.amanzi.asn1.parser.lexer.impl.Sequence;
 import org.amanzi.asn1.parser.token.IToken;
 import org.apache.commons.lang3.ArrayUtils;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
- * TODO Purpose of 
- * <p>
- *
- * </p>
+ * Tests on ClassDefinition Logic
+ * 
  * @author Nikolay Lagutko (nikolay.lagutko@amanzitel.com)
  * @since 1.0.0
  */
 public class ClassDefinitionLogicTest {
+    
+    @Rule
+    public ExpectedException syntaxException = ExpectedException.none();
     
     private static final String[] ENUMERATED = new String[] { "{", "enum1", ",", "enum2", "}" };
     
@@ -49,13 +58,23 @@ public class ClassDefinitionLogicTest {
     private IStream<IToken> getTokenStream(String[] firstElements, String... lastElements) {
         return new TestTokenStream(ArrayUtils.addAll(firstElements, lastElements));
     }
-
+    
+    private void prepareException(ErrorReason errorReason) {
+        syntaxException.expect(SyntaxException.class);
+        syntaxException.expectMessage(containsString(errorReason.getMessage()));
+    }
+    
+    private ClassDefinition run(IStream<IToken> tokenStream) throws SyntaxException {
+        ClassDefinitionLogic logic = new ClassDefinitionLogic(tokenStream);
+        
+        return logic.parse(new ClassDefinition());
+    }
+    
     @Test
     public void testCheckCorrectClassName() throws Exception {
         IStream<IToken> tokenStream = getEnumaredTokenStream("ClassDefinition");
         
-        ClassDefinitionLogic logic = new ClassDefinitionLogic(tokenStream);
-        ClassDefinition result = logic.parse(new ClassDefinition());
+        ClassDefinition result = run(tokenStream);
         
         assertEquals("unexpected name of Class Definition", result.getClassName(), "ClassDefinition");
     }
@@ -64,8 +83,7 @@ public class ClassDefinitionLogicTest {
     public void testCheckDescriptionNotNull() throws Exception {
         IStream<IToken> tokenStream = getEnumaredTokenStream("ClassDefinition");
         
-        ClassDefinitionLogic logic = new ClassDefinitionLogic(tokenStream);
-        ClassDefinition result = logic.parse(new ClassDefinition());
+        ClassDefinition result = run(tokenStream);
         
         assertNotNull("ClassDescription should exist", result.getClassDescription());
     }
@@ -74,8 +92,7 @@ public class ClassDefinitionLogicTest {
     public void testCheckCorrectEnumeratedDescriptionType() throws Exception {
         IStream<IToken> tokenStream = getEnumaredTokenStream("ClassDefinition");
         
-        ClassDefinitionLogic logic = new ClassDefinitionLogic(tokenStream);
-        ClassDefinition result = logic.parse(new ClassDefinition());
+        ClassDefinition result = run(tokenStream);
         
         assertEquals("unexpected type of Class Description", result.getClassDescription().getType(), ClassDescriptionType.ENUMERATED);
         assertEquals("unexpected class of Class Description", result.getClassDescription(), Enumerated.class);
@@ -85,11 +102,63 @@ public class ClassDefinitionLogicTest {
     public void testCheckCorrectSequenceDescriptionType() throws Exception {
         IStream<IToken> tokenStream = getSequenceTokenStream("ClassDefinition");
         
-        ClassDefinitionLogic logic = new ClassDefinitionLogic(tokenStream);
-        ClassDefinition result = logic.parse(new ClassDefinition());
+        ClassDefinition result = run(tokenStream);
         
         assertEquals("unexpected type of Class Description", result.getClassDescription().getType(), ClassDescriptionType.SEQUENCE);
         assertEquals("unexpected class of Class Description", result.getClassDescription(), Sequence.class);
     }
-
+    
+    @Test
+    public void testCheckCorrectDescriptionType() throws Exception {
+        for (ClassDescriptionType descriptionType : ClassDescriptionType.values()) {
+            IStream<IToken> tokenStream = null;
+            Class<? extends IClassDescription> descriptionClass = null;
+            
+            String definitionName = "ClassDefinition";
+            
+            switch (descriptionType) {
+            case ENUMERATED:
+                descriptionClass = Enumerated.class;
+                tokenStream = getEnumaredTokenStream(definitionName);
+                break;
+            case SEQUENCE:
+                descriptionClass = Sequence.class;
+                tokenStream = getSequenceTokenStream(definitionName);
+                break;
+            }
+            
+            ClassDefinition definition = run(tokenStream);
+            
+            assertNotNull("class description should not be null", definition.getClassDescription());
+            assertEquals("unexpected Class Description type", descriptionType, definition.getClassDescription().getType());
+            assertEquals("unexpected Class Description class", descriptionClass, definition.getClassDescription().getClass());
+        }
+    }
+    
+    @Test
+    public void testCheckUnexpectedEndOfStreamException() throws Exception {
+        prepareException(ErrorReason.UNEXPECTED_END_OF_STREAM);
+        
+        IStream<IToken> tokenStream = new TestTokenStream("WrongClass", "::=");
+        
+        run(tokenStream);
+    }
+    
+    @Test
+    public void testCheckNotSupportedTokenException() throws Exception {
+        prepareException(ErrorReason.TOKEN_NOT_SUPPORTED);
+        
+        IStream<IToken> tokenStream = new TestTokenStream(",", "::=");
+        
+        run(tokenStream);
+    }
+    
+    @Test
+    public void testCheckUnexpectedTokenInLexem() throws Exception {
+        prepareException(ErrorReason.UNEXPECTED_TOKEN_IN_LEXEM);
+        
+        IStream<IToken> tokenStream = new TestTokenStream("ClassDefinitionName", "SEQUENCE");
+        
+        run(tokenStream);
+    }
 }
